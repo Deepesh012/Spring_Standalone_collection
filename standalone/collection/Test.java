f7f74b9f6ae235aa98743cafe7e7902cb5b3b88e
Deepesh Thakur <128272082+Deepesh012@users.noreply.github.com>
2024-05-17 17:09:21 +0530

    Add files via upload

---
+++ standalone/collection/Test.java
@@ -0,0 +1,18 @@
+package com.springcore.standalone.collection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/standalone/collection/aloneconfig.xml");
+		Person person1 = context.getBean("person1",Person.class);
+		System.out.println(person1);
+		System.out.println(person1.getFriends().getClass().getName());
+		System.out.println("++++++++++++++++++++++++++++++++");
+		System.out.println(person1.getFeestructure());
+		System.out.println(person1.getFeestructure().getClass().getName());
+	}
+	
+}
